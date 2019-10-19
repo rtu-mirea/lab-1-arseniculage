56c86e5b6980b1384783682f79ad99aa4ebb430a
arseniculage <worknavo@mail.ru>
2019-10-19 10:38:19 +0300

    minor tweaks

--- lab1/src/com/company/Menu.java
+++ lab1/src/com/company/Menu.java
@@ -9,11 +9,13 @@ public class Menu {
         Array arr = new Array(0);
 
         Scanner in = new Scanner(System.in);
-        System.out.println("1.input array  \n2. append to array \n3. pop from array \n4. find if max bigger than input \n5. order by last digit \n6 diplay array");
 
         int menu_input;
         while (true) {
+            System.out.println("1.input array  \n2. append to array \n3. pop from array \n4. find if max bigger than input \n5. order by last digit \n6 diplay array");
+
             menu_input = in.nextInt();
+
             switch (menu_input) {
                 case (1):
                     int size = in.nextInt();
@@ -22,17 +24,25 @@ public class Menu {
                 case (2):
                     int input = in.nextInt();
                     arr.append(input);
+                    break;
                 case (3):
                     int index = in.nextInt();
                     arr.pop(index);
+                    break;
                 case (4):
                     arr.check_maxBiggerInput(in);
+                    break;
                 case (5):
                     arr.orderByLastDigit();
+                    break;
                 case (6):
                     arr.print_string();
+                    break;
 
             }
+            System.out.println();
+
+
         }
 
 
